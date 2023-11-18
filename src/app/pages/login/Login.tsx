c0c2e17716ca0d70e287f6e7bb2b7a1861ecc1ab
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-18 19:30:22 -0300

    make login and save token

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
+import { AuthenticationUseCase } from "../../../usecases/protocols/authentication-protocols";
+
+import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
 
 type Props = {
-  validation: any
-  authentication: any
-}
+  authentication: AuthenticationUseCase;
+  cookies: Cookies;
+};
 
-export function Login({ authentication, validation }: Props) {
+export function Login({ authentication, cookies }: Props) {
+  /**
+   *  escolhi usar useRef para evitar re-renders desnecessários ao digitar cada tecla do teclado no formulário para ajudar a otimizar o desempenho,
+   *  já que useRef permite acessar os valores dos campos de forma direta,
+   *  sem acionar re-renderizações em cada alteração.
+   */
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (emailRef.current && passwordRef.current) {
+      try {
+        const response = await authentication.execute({
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+        });
+        cookies.set("access_token", response.data.accessToken, {
+          path: "/",
+          httpOnly: true,
+          secure: true,
+        });
+        navigate('/home/tools')
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <main className="flex">
       <div data-testid="form" className="w-[80vw] h-[95vh] mt-[60px] p-10">
@@ -13,13 +47,18 @@ export function Login({ authentication, validation }: Props) {
           <p>FEMAQUA</p>
           <p>Ferramentas maravilhosas que adoro</p>
         </div>
-        <form data-testid="form" className="m-auto w-[90%] h-[50%]">
+        <form
+          data-testid="form"
+          className="m-auto w-[90%] h-[50%]"
+          onSubmit={handleSubmit}
+        >
           <label>Email</label>
           <input
             type="email"
             name="email"
             placeholder="Digite seu e-mail"
             className="block mb-2 w-[70%] p-2 border-solid border-2 border-black"
+            ref={emailRef}
           />
           <label>Senha</label>
           <input
@@ -27,9 +66,13 @@ export function Login({ authentication, validation }: Props) {
             name="password"
             placeholder="Digite sua senha"
             className="block mb-2 w-[70%] p-2 border-solid border-2 border-black"
+            ref={passwordRef}
           />
           <div className="w-[70%] flex justify-end">
-            <button className="p-3 w-[100px] bg-[#365DF0] text-white rounded cursor-pointer">
+            <button
+              type="submit"
+              className="p-3 w-[100px] bg-[#365DF0] text-white rounded cursor-pointer"
+            >
               Entrar
             </button>
           </div>
