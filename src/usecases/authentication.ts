bd693f2f87144c41ff3252db5fffe77bc0b32583
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-18 18:48:34 -0300

    usecase implement their protocol

--- src/usecases/authentication.ts
+++ src/usecases/authentication.ts
@@ -2,27 +2,23 @@ import { HttpStatusCode } from "axios";
 import { AuthenticationRepo } from "./protocols/auth/authentication-protocol";
 import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
 import { UnexpectedError } from "./errors/unexpected-error";
+import { Authentication, AuthenticationUseCase } from "./protocols/authentication-protocols";
 
-export class AccountAuthentication {
+export class AccountAuthentication implements AuthenticationUseCase {
   constructor(
     private readonly url: string,
     private readonly authentication: AuthenticationRepo
   ) {}
 
-  async auth() {
+  async execute(params: Authentication.Param) {
     const remoteAccountAuth = await this.authentication.auth({
       url: this.url,
-      data: {
-        email: "any_user_email@mail.com",
-        password: "any_user_password",
-      },
+      data: params,
     });
 
     switch (remoteAccountAuth.status) {
-      case HttpStatusCode.Ok:
-        return remoteAccountAuth;
-      case HttpStatusCode.Forbidden:
-        throw new InvalidCredentialsError();
+      case HttpStatusCode.Ok: return remoteAccountAuth;
+      case HttpStatusCode.Forbidden: throw new InvalidCredentialsError();
       default:
         throw new UnexpectedError();
     }
