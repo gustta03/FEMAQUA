3f75782753b0f68ee9bc81b23362ce0d8a6a65e2
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-18 19:28:49 -0300

    add makeCookie to inject depedencies in Login page

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import '../app/styles/index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/app-router'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
      <RouterProvider router={router} />
+     <Toaster />
   </React.StrictMode>,
 )
