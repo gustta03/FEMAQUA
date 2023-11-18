d846baa456cbe82fc9a0352600ae4b6feb0b2d5e
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-18 19:32:29 -0300

    inject cookie deps in login page

--- src/main/router/app-router.tsx
+++ src/main/router/app-router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../../app/pages/login/Login";
 import { makeAuthUseCase } from "../factories/usecase/make-auth-usecase";
+import { makeCookieAuth } from "../factories/make-cookie";
 
 const router = createBrowserRouter([
   {
     path: '/login',
-    element: <Login authentication={makeAuthUseCase()} />, 
+    element: <Login authentication={makeAuthUseCase()} cookies={makeCookieAuth()} />, 
   },
 ]);
 
