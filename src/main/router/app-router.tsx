f33aa1323141605846c25bc33f4f5cc4fd815439
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-18 23:30:50 -0300

    add app router tools page

--- src/main/router/app-router.tsx
+++ src/main/router/app-router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../../app/pages/login/Login";
 import { makeAuthUseCase } from "../factories/usecase/make-auth-usecase";
 import { makeCookieAuth } from "../factories/make-cookie";
+import Tool from "../../app/pages/tools/tools";
 
 const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login authentication={makeAuthUseCase()} cookies={makeCookieAuth()} />, 
+  },
+  {
+    path: '/home/tools',
+    element: <Tool addTool={undefined} />, 
   },
 ]);
 
---
+++ tailwind.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class'
+} satisfies Config;
