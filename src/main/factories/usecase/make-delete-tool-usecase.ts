23892e54f3e34fa7a1fcc014aa0c61d9c4403184
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-19 02:47:11 -0300

    add makeDeleteTool factory

---
+++ src/main/factories/usecase/make-delete-tool-usecase.ts
@@ -0,0 +1,6 @@
+import { DeleteTool } from "../../../usecases/delete-tool-by-id";
+import { makeApiUrl } from "../infra/http/make-api-url";
+import { makeToolRepo } from "../infra/repos/make-tool-repository";
+
+export const makeDeleteTool = () =>
+    new DeleteTool(makeApiUrl("tools/"), makeToolRepo());
