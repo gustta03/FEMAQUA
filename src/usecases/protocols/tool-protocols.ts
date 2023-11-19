e314a55e62f0b72398740918d4233ea46b06458c
Gustavo Alves <gustavoalves03.dev@gmail.com>
2023-11-18 23:17:53 -0300

    add tool protocols

---
+++ src/usecases/protocols/tool-protocols.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Tool } from "../../domain/models/tools-model";
+
+export interface SaveTool {
+  insert(params: Tool.Param): Promise<Tool.Response>;
+  load(params: Tool.Param): Promise<Tool.Response>;
+}
+
+export interface Tools {
+  execute(params: Tool.Param): Promise<Tool.Response | undefined>;
+}
+
+/* eslint-disable @typescript-eslint/no-namespace */
+
+export namespace Tool {
+  export type Param = {
+    url: string;
+    token: string;
+    data: any
+  };
+
+  export type Response = {
+    status: number;
+    data: Tool;
+  };
+}
